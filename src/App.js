e43fec6a107dc51120210fa4cc8f3b8ed52fdb1d
HADIJAH KYAMPEIRE <hadijahkyampeire@HADIJAHs-MacBook-Pro.local>
2018-10-20 17:55:17 +0300

    Implement a data stastic React/D3 app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import BarChart from './myD3App/BarChart';
+import WorldMap from './myD3App/WorldMap';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App'>
+      <div className='App-header'>
+      <h2>My-d3 dashboard</h2>
+      </div>
+      <div>
+     <BarChart data={[5,10,1,3]} size={[500,500]} />
+      <WorldMap/>
+      </div>
       </div>
     );
   }
